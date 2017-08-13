48b583092df3197233f6bf54579e18828d7959a0
abhilashsharma <dx.abhilash@gmail.com>
2017-08-13 17:49:59 +0530

    Computing costs

--- src/main/java/in/dream_lab/goffish/godb/PathQueryCostModel.java
+++ src/main/java/in/dream_lab/goffish/godb/PathQueryCostModel.java
@@ -1188,7 +1188,7 @@ public static void main(String[] args){
             Args=sCurrentLine;
             System.out.println(Args);
             init(Args);
-            computeCoeff();
+            computeNWFixed();
             clear();  
     }
     br.close();
