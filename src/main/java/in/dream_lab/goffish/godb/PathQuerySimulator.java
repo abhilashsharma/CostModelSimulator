46814b2deb683154ab6325b4313b416576787a3b
abhilash <abhilashsharma>
2017-10-09 10:47:01 +0530

    removed loggin

--- src/main/java/in/dream_lab/goffish/godb/PathQuerySimulator.java
+++ src/main/java/in/dream_lab/goffish/godb/PathQuerySimulator.java
@@ -1299,7 +1299,7 @@ static int R(int _sgid,int _sp,int _step,int[][][] _n,int[][][] _l,int[][][] _s,
     	int h=(_step+1)/2;
     	Step s=path.get(2*h);
 		String property = s.property;
-		System.out.println("H:"+ 2*h+" prop:"+property+" val:"+s.value);
+//		System.out.println("H:"+ 2*h+" prop:"+property+" val:"+s.value);
 		String value=s.value.toString();
     	res=(int) (N(_sgid,_sp,h,_n)*hueristics.avgRemoteDeg(property, value, true, true));
     }
