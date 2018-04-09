21dcca6729cc15f829609026a9fb93ff7faee45f
abhilash <abhilashsharma>
2018-04-09 22:52:35 +0530

    logging Coefficient

--- src/main/java/in/dream_lab/goffish/godb/PathQuerySimulatorLogging.java
+++ src/main/java/in/dream_lab/goffish/godb/PathQuerySimulatorLogging.java
@@ -1441,7 +1441,7 @@ public static void main(String[] args){
 	            Args=sCurrentLine;
 	            System.out.println(Args);
 	            init(Args);
-	            computeNWFixed();
+	            computeCoeff();
 	            clear();  
 	    }
 	    br.close();
